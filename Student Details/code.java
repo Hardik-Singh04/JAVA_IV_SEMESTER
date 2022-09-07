68b5b3cf4423b45505f08ed52bb4d553728eceff
Hardik Singh <91072109+Hardik-Singh04@users.noreply.github.com>
2022-09-07 20:26:41 +0530

    Create code.java
    : 1 - Create a class “Student” having following instance variables and
    methods.
    Instance variables: ID, Name, Branch and university
    Method: setDetails() and showDetails().
    The setDetails() method sets the values of ID, Name, Branch and University.
    And showDetails() method shows the value of each field.

---
+++ Student Details/code.java
@@ -0,0 +1,42 @@
+import java.util.*;
+public class Student
+{
+String Name,Branch,University;
+int Id;
+void setDetails(String Name,int Id,String University,String Branch)
+{
+this.Name=Name;
+this.Id=Id;
+this.University=University;
+this.Branch=Branch;
+}
+void showDetails()
+{
+System.out.println();
+System.out.println();
+System.out.println("Name : "+Name);
+System.out.println("Student Id :"+Id);
+System.out.println("Branch : "+Branch);
+System.out.println("University : "+University);
+}
+public static void main(String args[])
+{
+String Name,Branch,University;
+int Id;
+Scanner in=new Scanner(System.in);
+System.out.print("Enter the Student Id : ");
+Id=in.nextInt();
+Name : Hardik Singh Roll Number : 21 Section : A
+- 2 -
+in.nextLine();
+System.out.print("Enter the University name : ");
+University=in.nextLine();
+System.out.print("Enter the name of the student: ");
+Name=in.nextLine();
+System.out.print("Enter the Branch : ");
+Branch=in.nextLine();
+Student a=new Student();
+a.setDetails(Name,Id,University,Branch);
+a.showDetails();
+}
+}
