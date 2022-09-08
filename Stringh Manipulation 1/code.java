7243e424e593f21b142e5d263f65deffa9559064
Hardik Singh <91072109+Hardik-Singh04@users.noreply.github.com>
2022-09-09 04:50:44 +0530

    Create code.java

---
+++ Stringh Manipulation 1/code.java
@@ -0,0 +1,9 @@
+/*6 - Apply following functions on the String "Java".
+(i) Try to concat "Welcome" and write down your observation.
+(ii) Find character at index 1
+(iii) Find index of first 'a'.
+(iv) Find index of second 'a'
+(v) Compare "Java" to "JAVA"
+(vi) Compare "Java" to "JAVA" ignoring the case
+(vii) Find the index of first 'a' from last*/
+
