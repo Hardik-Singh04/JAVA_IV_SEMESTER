b53a841853c76ca872da8f06110721b9e642557f
Hardik Singh <91072109+Hardik-Singh04@users.noreply.github.com>
2022-09-16 18:49:16 +0530

    Create code.java

---
+++ Low Balance Exception/code.java
@@ -0,0 +1,4 @@
+/* Create LowBalanceException that occurs when user tries to 
+withdraw some amount that is greater than his current bank balance. To 
+withdraw you must write a void withdrawal (int amount) method.*/
+
