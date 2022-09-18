38172074244df93787529c69c20b7cbe10426a1b
Hardik Singh <91072109+Hardik-Singh04@users.noreply.github.com>
2022-09-18 19:19:09 +0530

    Update code.java

--- Read Write Keyboard/code.java
+++ Read Write Keyboard/code.java
@@ -1,3 +1,22 @@
-/*
-
-*/
+/*- Write a program that take your name from keyboard and writes 
+in some text file.*/
+import java.util.*;
+import java.io.*;
+public class Input
+{
+ public static void main(String args[]) throws IOException
+ {
+ Scanner in=new Scanner(System.in);
+ FileWriter fw=new FileWriter("file1.txt");
+ BufferedWriter bw=new BufferedWriter(fw);
+ System.out.print("Enter your name : ");
+ String s=in.nextLine();
+ int n=s.length();
+ for(int i=0;i<n;i++)
+ {
+ bw.write(s.charAt(i));
+ }
+ bw.close();
+ fw.close();
+ }
+}
