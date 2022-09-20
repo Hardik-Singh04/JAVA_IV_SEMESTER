efaf5fa5432595b35ceb3e4786ba6c31d959a019
Hardik Singh <91072109+Hardik-Singh04@users.noreply.github.com>
2022-09-20 19:08:45 +0530

    Create code.java

---
+++ Multithreading Time Delay/code.java
@@ -0,0 +1,6 @@
+/*Write a class Display having synchronized void wish(String) 
+methods that wishes hello to given string name. Between printing hello and 
+provided string name apply delay of 500 milliseconds. Suppose multiple threads 
+are there and they are trying to access this wish() method concurrently on 
+different objects then irregular output will be there. Write this application in 
+such a way so that output becomes regular.*/
