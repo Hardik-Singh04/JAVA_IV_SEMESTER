6c7fbf7d0aacc1de41b951f233f3d30da3539553
Hardik Singh <91072109+Hardik-Singh04@users.noreply.github.com>
2022-09-20 19:09:50 +0530

    Update code.java

--- Multithreading Time Delay/code.java
+++ Multithreading Time Delay/code.java
@@ -4,3 +4,49 @@ provided string name apply delay of 500 milliseconds. Suppose multiple threads
 are there and they are trying to access this wish() method concurrently on 
 different objects then irregular output will be there. Write this application in 
 such a way so that output becomes regular.*/
+class Display
+{
+public static synchronized void wish(String s)
+{
+for(int i=0;i<10;i++)
+{
+System.out.print("Hello :");
+try
+{
+Thread.sleep(500);
+}
+catch(Exception e)
+{
+System.out.println(e);
+}
+System.out.println(s);
+}
+}
+}
+class Mythread extends Thread
+{
+Display a;
+String name;
+Mythread(Display a,String name)
+{
+this.a=a;
+this.name=name;
+}
+public void run()
+{
+a.wish(name);
+}
+}
+public class StartSync
+{
+public static void main(String args[])
+{
+Display d=new Display();
+Display d1=new Display();
+Mythread t=new Mythread(d,"Hardik");
+t.start();
+Mythread t1=new Mythread(d1,"Rahul");
+t1.start();
+}
+}
+
